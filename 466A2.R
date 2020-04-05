79a782e52bc1bc54e378c38948f4ae7e325bffb1
shawnparsons <60654663+shawnparsons@users.noreply.github.com>
2020-04-05 15:57:54 -0400

    Add files via upload

---
+++ 466A2.R
@@ -0,0 +1,9 @@
+install.packages("powerplus")
+library("powerplus")
+
+mat = matrix(c(0.8,0.1,0.1,0,0.1,0.5,0.3,0,0.1,0.2,0.3,0,0,0.2,0.3,1), ncol = 4, nrow = 4)
+mat
+Matpow(mat,1/12)
+
+a = mat %^%1/12
+a
